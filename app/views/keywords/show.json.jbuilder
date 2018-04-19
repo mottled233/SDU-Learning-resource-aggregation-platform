69d966951d3dcf63ffad05ceee10e6ddf91616c3
qdwwwzzy <w-z-y1997@163.com>
2018-04-20 00:21:39 +0800

    change the keyword

---
+++ app/views/keywords/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @keyword, :id, :name, :course_id, :created_at, :updated_at
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180418125412) do
+ActiveRecord::Schema.define(version: 20180419153152) do
 
   create_table "departments", force: :cascade do |t|
     t.string   "name"
@@ -19,11 +19,38 @@ ActiveRecord::Schema.define(version: 20180418125412) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "dept_courses", force: :cascade do |t|
+    t.integer  "dept_id"
+    t.string   "course_id"
+    t.string   "integer"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "keywords", force: :cascade do |t|
     t.string   "name"
-    t.string   "Course_ID_belong"
-    t.datetime "created_at",       null: false
-    t.datetime "updated_at",       null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "knowledges", force: :cascade do |t|
+    t.integer  "course_id"
+    t.integer  "user_id"
+    t.string   "type"
+    t.string   "content"
+    t.integer  "good"
+    t.integer  "bad"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["course_id"], name: "index_knowledges_on_course_id"
+    t.index ["user_id"], name: "index_knowledges_on_user_id"
+  end
+
+  create_table "teaching_relationships", force: :cascade do |t|
+    t.integer  "teacher_id"
+    t.integer  "course_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
