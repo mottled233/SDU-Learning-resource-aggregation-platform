dfd9d9126e687cf4457c64a9465d556e225fb09f
mottled233 <mottled233@sohu.com>
2018-05-01 14:35:56 +0000

    refactor login page

--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,7 @@ Rails.application.routes.draw do
   get '/home', to: 'static_pages#home'
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
+  get '/reg', to: 'user#new'
   delete '/logout', to: 'sessions#destroy'
   get '/logout', to: 'sessions#destroy'
   
