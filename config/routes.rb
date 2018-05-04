ba58455daf7257ed251587d8d17568413e5e155e
Shao Chanxu <m17864154809@163.com>
2018-05-04 14:44:33 +0000

    2018/5/4

--- config/routes.rb
+++ config/routes.rb
@@ -26,6 +26,18 @@ Rails.application.routes.draw do
  
  
 # start:scx's routes
+ get "/resources/file_download",to: "resources#file_download",as:"resource_file_download"
+ 
+ get "/courses/:course_id/questions", to: "courses#questions_index", as: "course_questions"
+ get "/courses/:course_id/blogs", to: "courses#blogs_index", as: "course_blogs"
+ get "/courses/:course_id/resources", to: "courses#resources_index", as: "course_resources"
+
+ get "questions/new",to: "questions#new",as:"question_new"
+ get "blogs/new",to: "blogs#new",as:"blog_new"
+ get "resources/new",to: "resources#new",as:"resource_new"
+  
+# 评论
+ get 'knowledges/reply_show',to: "knowledges#reply_show",as: "reply_show"
 
  resources :courses
  resources :questions
@@ -33,14 +45,8 @@ Rails.application.routes.draw do
  resources :resources
  resources :replies
 
- get "/courses/:course_id/questions", to: "courses#questions_index", as: "course_questions"
- get "/courses/:course_id/blogs", to: "courses#blogs_index", as: "course_blogs"
- get "/courses/:course_id/resources", to: "courses#resources_index", as: "course_resources"
+ 
 
- get "questions/new",to: "questions#new",as:"question_new"
- get "blogs/new",to: "blogs#new",as:"blog_new"
-# 评论
- get 'knowledges/reply_show',to: "knowledges#reply_show",as: "reply_show"
 # 点赞/踩/关注
  match "/", to: "knowledges#add_evalute", as: "add_evalute", via: :post
  match '/focus/ajax', to: 'knowledges#focus',as: "focus", via: :get
@@ -52,15 +58,6 @@ Rails.application.routes.draw do
  match '/good_add_bad_sub/ajax', to: 'knowledges#good_add_bad_sub',as: "good_add_bad_sub", via: :get
  match '/good_sub_bad_add/ajax', to: 'knowledges#good_sub_bad_add',as: "good_sub_bad_add", via: :get
  
- # match "/blogs", to: "knowledges#add_evalute", as: "add_evalute", via: :post
- # match '/blogs/focus/ajax', to: 'knowledges#focus',as: "focus", via: :get
- # match '/blogs/unfocus/ajax', to: 'knowledges#unfocus',as: "unfocus", via: :get
- # match '/blogs/good_add/ajax', to: 'knowledges#good_add',as: "good_add", via: :get
- # match '/blogs/bad_add/ajax', to: 'knowledges#bad_add',as: "bad_add", via: :get
- # match '/blogs/good_sub/ajax', to: 'knowledges#good_sub',as: "good_sub", via: :get
- # match '/blogs/bad_sub/ajax', to: 'knowledges#bad_sub',as: "bad_sub", via: :get
- # match '/blogs/good_add_bad_sub/ajax', to: 'knowledges#good_add_bad_sub',as: "good_add_bad_sub", via: :get
- # match '/blogs/good_sub_bad_add/ajax', to: 'knowledges#good_sub_bad_add',as: "good_sub_bad_add", via: :get
 # end:scx's routes
 
  get '/test', to: 'static_pages#test'
