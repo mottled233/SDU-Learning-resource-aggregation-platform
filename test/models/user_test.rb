e924ec0bf3e5ec4bb9ee70241e2872006a81a9e0
mottled233 <mottled233@sohu.com>
2018-05-10 15:29:23 +0000

    user edit view completed

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -15,7 +15,39 @@ class UserTest < ActiveSupport::TestCase
   
   test "should be valid" do
     assert @student_user.valid?
-    @user.valid?
-    puts @user.errors.full_messages
+  end
+  
+  test "should detect department error" do
+    @student_user.department = "软件学院"
+    @student_user.valid?
+    assert @student_user.errors[:department].empty?
+    @student_user.department = '没有学院'
+    @student_user.valid?
+    assert_not @student_user.errors[:department].empty?
+  end
+  
+  test "should detect specialities error" do
+    @student_user.department = "软件学院"
+    @student_user.speciality = "软件工程"
+    @student_user.valid?
+    assert @student_user.errors[:speciality].empty?
+    @student_user.department = "数学院"
+    @student_user.speciality = "软件工程"
+    @student_user.valid?
+    assert_not @student_user.errors[:speciality].empty?
+    @student_user.speciality = ""
+    @student_user.valid?
+    assert @student_user.errors[:speciality].empty?
+  end
+  
+  test "should auto set department_id" do
+    @student_user.department_id = nil
+    @student_user.speciality_id = nil
+    @student_user.department = "软件学院"
+    @student_user.speciality = "软件工程"
+    assert @student_user.save!
+    assert_equal @student_user.department_id, Department.find_by(name: @student_user.department).id
+    assert_equal @student_user.speciality_id, Speciality.find_by(name: @student_user.speciality).id
+    
   end
 end
