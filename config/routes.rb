543cfb79d4bc4a18b2a97e72f2ec26fd3e93a8b5
719 <baka719@qq.com>
2018-05-14 08:57:26 +0000

    Add route

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
 
-  get 'searches/index'
-
-  get 'searches/result'
-
  resources :departments
   get 'departments/new'
 
@@ -19,7 +15,7 @@ Rails.application.routes.draw do
   get '/reg', to: 'users#new'
   delete '/logout', to: 'sessions#destroy'
   get '/logout', to: 'sessions#destroy'
-  
+  get '/search', to: 'searches#index'
   resources :users
   
   post '/users/delete/:id', to: 'users#destroy', as:"delete_user"
