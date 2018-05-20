be3767b3d9f734290805e9137284d6f4373471b1
719 <baka719@qq.com>
2018-05-20 14:34:49 +0000

    Fix homepage bugs

--- app/assets/javascripts/js/static_pages.js
+++ app/assets/javascripts/js/static_pages.js
@@ -29,8 +29,12 @@ function changecolor(id)
 				console.log(h.response);
 				var o=JSON.parse(h.response);
 				if (o.status=="200"){
-					console.log(o.data[0]);
 					document.getElementById("row"+id.substring(1,2)).innerHTML="";
+					var type="";
+					if (id.substring(1,2)=="1")
+						type="blogs";
+					else
+						type="resources";
 					for (var j=0;j<=3;++j)
 					{
 						var span3 = document.createElement("div");
@@ -55,6 +59,7 @@ function changecolor(id)
 						var calendar = document.createElement("span");
 						calendar.setAttribute("class","icon-calendar");
 						a.innerHTML = o.data[j].title;
+						a.setAttribute("href","http://"+document.domain+"/"+type+"/"+o.data[j].id);
 						h2.appendChild(a);
 						small1.innerHTML = "&nbsp;&nbsp;&nbsp;&nbsp";
 						small1.appendChild(book);
@@ -103,13 +108,6 @@ function dropdownOnClick(e,id)
 		document.getElementById('dropdown'+id).setAttribute("data-id",e.target.getAttribute("data-id"));
 		if (document.getElementById('dropdown'+(parseInt(id)+1).toString()))
 		{
-			if (document.getElementById('dropdown'+(parseInt(id)+2).toString()))
-			{
-				document.getElementById('dropdown'+(parseInt(id)+2).toString()).parentNode.style.display="none";
-				document.getElementById('ul'+(parseInt(id)+2).toString()).innerHTML="";
-			}
-			document.getElementById('dropdown'+(parseInt(id)+1).toString()).parentNode.style.display="inline";
-				document.getElementById('ul'+(parseInt(id)+1).toString()).innerHTML="";
 				
 			var x={};
 			x.authenticity_token=$("meta[name='csrf-token']").attr("content");
@@ -128,6 +126,13 @@ function dropdownOnClick(e,id)
 				if (o.status=="200"){
 					for (var i=0;i<o.count;++i)
 					{
+						if (document.getElementById('dropdown'+(parseInt(id)+2).toString()))
+						{
+							document.getElementById('dropdown'+(parseInt(id)+2).toString()).parentNode.style.display="none";
+							document.getElementById('ul'+(parseInt(id)+2).toString()).innerHTML="";
+						}
+						document.getElementById('dropdown'+(parseInt(id)+1).toString()).parentNode.style.display="inline";
+							document.getElementById('ul'+(parseInt(id)+1).toString()).innerHTML="";
 						var newli=document.createElement("li");
 						newli.setAttribute("data-id",o.data[i].id);
 						if (o.data[i].name)
@@ -150,7 +155,7 @@ function dropdownOnClick(e,id)
 		}
 		if (id=='3')
 		{
-		  var url = "https://sdulrap-baka719.c9users.io/courses/"+e.target.getAttribute("data-id");
+		  var url = "http://"+document.domain+"/courses/"+e.target.getAttribute("data-id");
 			document.getElementById('goButton').style.display="inline";
 			document.getElementById('goButton').setAttribute('onclick','window.open("'+url+'")');
 		}
