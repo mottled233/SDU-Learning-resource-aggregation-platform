265c52f3898af88b009e0a9ce4b252da698f4592
719 <baka719@qq.com>
2018-06-12 02:40:10 +0000

    Add score calculate

--- app/jobs/score_calculate_job.rb
+++ app/jobs/score_calculate_job.rb
@@ -3,14 +3,14 @@ class ScoreCalculateJob < ApplicationJob
 
   def perform(*args)
     Knowledge.all.each do |k|
-      visit = (k.visit_count>0 ? k.visit_count : 1)
+      visit = k.visit_count
       focus = k.followers.length
       comments = k.getAllReplies.length
-      good_per = k.good.to_f/(k.good+k.bad)
+      good_per = k.good+k.bad>0?(k.good.to_f/(k.good+k.bad)) : 0
       
       visit_cor = 1+good_per*0.5
-      focus_cor = 15+20*[focus.to_f/visit,0.25].min
-      comment_cor = 10*(visit+15*focus)/(visit+15*focus+comments)
+      focus_cor = visit>0?(15+20*[focus.to_f/visit,0.25].min) : 0
+      comment_cor = visit>0?(10*(visit+15*focus)/(visit+15*focus+comments)) : 0
       
       score = visit*visit_cor+focus*focus_cor+comments*comment_cor
       
